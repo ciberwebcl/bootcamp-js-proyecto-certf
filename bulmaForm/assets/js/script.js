b69a943078140b476dd4ed1550500737eea9ea4f
ciberwebcl <135287760+ciberwebcl@users.noreply.github.com>
2023-09-28 17:06:31 -0300

    initial commit: base proyecto

--- bulmaForm/assets/js/script.js
+++ bulmaForm/assets/js/script.js
@@ -1,7 +1,5 @@
-
-
-
-/* :::::: Dropdown Menu :::::: */
+// 1. Creamos una función que se habilita cuando el DOM se carga completamente addEventListener('DOMContentLoaded', function() {})
+// 2. Esta función lo que hace es hacer funcionar el botón de hamburguesa que se crea en el navbar en su versión mobile
 document.addEventListener('DOMContentLoaded', function() {
 
     const menus = document.querySelectorAll('.navbar-burger');
@@ -22,8 +20,56 @@ document.addEventListener('DOMContentLoaded', function() {
 
     }
 
-
 });
 
+// <--- Aquí debes escribir la funcionalidad asociada al formulario
+
+// Creamos una Función Flecha que se ejecutará al dar click en el botón
+const formSubmitted = () => {
+
+    // Capturamos el botón con JavaScript
+    const boton = document.getElementById('button_submit_form');
+    console.log('1. Botón Capturado');
+
+    // Deshabilitamos el Botón agregandole el atributo "disabled"
+    boton.setAttribute('disabled', '') ;
+    console.log('2. Botón Deshabilitado');
+
+    // Agregamos la clase de bulma "is-loading" para agregar un efecto de cargando ...
+    boton.classList.add('is-loading');
+    console.log('3. Animación "Cargando ..."');
+
+    // Ejecutamos nuestro SweetAlert2 Personalizado durante 3 segundos
+    Swal.fire({position: 'center',icon: 'success',title: '💪🏻 Reserva Enviada', text: '📩 Instrucciones en tu correo electrónico', showConfirmButton: false, timer: 3000 }) ;
+    console.log('4. SweetAlert2 Personalizado');
+
+    // Se lo quitamos después de 3.5 Segundos ...
+    setTimeout(()=> {
+
+        // Removemos la clase "is-loading" para que deje de cargar
+        boton.classList.remove('is-loading');
+        console.log('5. Clase "is-loading" Removida');
+
+        // Volvemos a habilitar el botón removiendo el atributo "disabled"
+        boton.removeAttribute('disabled');
+        console.log('6. Botón Nuevamente habilitado');
+
+        boton.innerHTML = 'Reserva Enviada 📨'
+        console.log('7. Texto del botón Remplazado');
+
+        console.log('8. Primer setTimeout() Completado');
+
+    }, 3500 /* Este es el tiempo en milisegundos */ );
 
 
+
+    // Si quisieramos Redireccionar a una URL después de 4 segundos ...
+    setTimeout(()=> {
+
+        // Redireccionamos al HOME
+        //window.location='/'
+
+    }, 4000 /* Este es el tiempo en milisegundos */ );
+
+
+}
